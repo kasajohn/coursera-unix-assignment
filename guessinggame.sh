24385b645aed5d3702276968ce645220d88edb35
kasajohn <kasagiannis@gmail.com>
2019-10-22 15:47:27 +0300

    added the script code

---
+++ guessinggame.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# file: guessinggame.sh
+
+echo "guess how many files are inside current directory... Type a number and then press Enter"
+read number
+
+countfiles() {
+	files=$(ls | wc -l)
+	echo $files
+}
+
+while [[ $number -ne $(countfiles) ]] || ! [[ $number =~ [0-9] ]]
+do
+	if [[ $number =~ [0-9] ]]
+	then
+		if [[ $number -lt $(countfiles) ]]
+		then
+			echo "the number you guessed is SMALLER than the number of files"
+			read number
+		elif [[ $number -gt $(countfiles) ]]
+		then
+			echo "the number you guessed is BIGGER than the number of files"
+			read number
+		else
+			echo "Congratulations you found the number of files inside this directory"
+			exit
+		fi
+	else
+		echo "only numeric characters are allowed"
+		read number
+	fi
+done
+echo "Congratulations you found the number of files inside this directory"
+
